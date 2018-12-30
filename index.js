d5e66547c338c22817eb5494bc26949f8f99eaf9
Arjun Patel <patel.arjun50@gmail.com>
2018-12-30 13:55:31 -0800

    basic web server

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    database : 'dbname',
-    user     : 'username',
-    password : 'password',
-});
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     database : 'dbname',
+//     user     : 'username',
+//     password : 'password',
+// });
+//
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error('Error connecting: ' + err.stack);
+//         return;
+//     }
+//
+//     console.log('Connected as id ' + connection.threadId);
+// });
+//
+// connection.query('SELECT * FROM employee', function (error, results, fields) {
+//     if (error)
+//         throw error;
+//
+//     results.forEach(result => {
+//         console.log(result);
+//     });
+// });
+//
+// connection.end();
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('Error connecting: ' + err.stack);
-        return;
-    }
+// content of index.js
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
 
-    console.log('Connected as id ' + connection.threadId);
-});
+const server = http.createServer(requestHandler)
 
-connection.query('SELECT * FROM employee', function (error, results, fields) {
-    if (error)
-        throw error;
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
 
-    results.forEach(result => {
-        console.log(result);
-    });
-});
-
-connection.end();
+  console.log(`server is listening on ${port}`)
+})
