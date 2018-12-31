4d2ba6228db2438298cd9f690ad4f5168b0bb5e4
Arjun Patel <patel.arjun50@gmail.com>
2018-12-30 23:06:59 -0800

    adding cors support

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
