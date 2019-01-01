4c2c257c265a874ddccd6d49429a637b9e051f94
Arjun Patel <patel.arjun50@gmail.com>
2019-01-01 11:35:50 -0800

    taking out comments

--- index.js
+++ index.js
@@ -73,15 +73,3 @@ app.get('/physicianloc', function (req, res) {
 
     });
 });
-
-
-// connection.query('SELECT * FROM employee', function (error, results, fields) {
-//     if (error)
-//         throw error;
-//
-//     results.forEach(result => {
-//         console.log(result);
-//     });
-// });
-//
-// connection.end();
